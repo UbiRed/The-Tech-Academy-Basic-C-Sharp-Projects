df734d249699053aa73711dafd17d3c8b8b5fa3b
Keaton K <keatonkupillas@yahoo.com>
2023-09-22 21:46:22 -0700

    Car Insurance Assignment

---
+++ basic_C#_programs/CarInsuranceApproval/CarInsuranceApproval/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Prompt the user for age
+        Console.WriteLine("What is your age?");
+        int age = int.Parse(Console.ReadLine());
+
+        // Prompt the user for DUI history
+        Console.WriteLine("Have you ever had a DUI? (true/false)");
+        bool hasDUI = bool.Parse(Console.ReadLine());
+
+        // Prompt the user for speeding tickets count
+        Console.WriteLine("How many speeding tickets do you have?");
+        int speedingTickets = int.Parse(Console.ReadLine());
+
+        // Check if the applicant qualifies for car insurance
+        bool isQualified = age > 15 && !hasDUI && speedingTickets <= 3;
+
+        
+        Console.WriteLine("Qualified? " + isQualified);
+        // Display results
+        Console.ReadLine();
+    }
+}
