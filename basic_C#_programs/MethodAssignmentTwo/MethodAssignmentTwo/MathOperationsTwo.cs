b38413314d797562e5ca6a4e7c0d707a5521944d
Keaton K <keatonkupillas@yahoo.com>
2023-09-27 14:27:46 -0700

    21 part 2 and methods part 2

---
+++ basic_C#_programs/MethodAssignmentTwo/MethodAssignmentTwo/MathOperationsTwo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class MathOperations
+{
+    // Method 1: Takes an integer, adds 5 to it, and results as an integer
+    public int PerformOperation(int number)
+    {
+        return number + 5;
+    }
+
+    // Method 2: Takes a decimal, multiplies it by 2, and results as an integer
+    public int PerformOperation(decimal number)
+    {
+        return (int)(number * 2);
+    }
+
+    // Method 3: Takes a string, converts it to an integer, subtracts 3, and results as an integer
+    public int PerformOperation(string input)
+    {
+        if (int.TryParse(input, out int number))
+        {
+            return number - 3;
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Couldn't convert to an integer.");
+            return 0; // Default value for invalid input
+        }
+    }
+}
---
+++ basic_C#_programs/MethodAssignmentTwo/MethodAssignmentTwo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Instantiate the MathOperations class
+        MathOperations mathOps = new MathOperations();
+
+        // Call the first method with an integer
+        int result1 = mathOps.PerformOperation(10);
+        Console.WriteLine($"Result of the first method: {result1}");
+
+        // Call the second method with a decimal
+        decimal decimalValue = 7.5M;
+        int result2 = mathOps.PerformOperation(decimalValue);
+        Console.WriteLine($"Result of the second method: {result2}");
+
+        // Call the third method with a string that equates to an integer
+        string inputString = "15";
+        int result3 = mathOps.PerformOperation(inputString);
+        Console.WriteLine($"Result of the third method: {result3}");
+
+        Console.ReadLine(); // Wait for user input to exit
+    }
+}
