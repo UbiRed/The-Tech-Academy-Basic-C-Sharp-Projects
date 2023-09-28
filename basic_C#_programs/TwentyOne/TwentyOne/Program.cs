1088cae7734fc104f545dffa323f4179d17e3725
Keaton K <keatonkupillas@yahoo.com>
2023-09-27 17:53:23 -0700

    Twenty one part 6

--- basic_C#_programs/TwentyOne/TwentyOne/Program.cs
+++ basic_C#_programs/TwentyOne/TwentyOne/Program.cs
@@ -10,10 +10,16 @@ namespace TwentyOne
     {
         static void Main(string[] args)
         {
+            TwentyOneGame game = new TwentyOneGame();
+            game.Players = new List<string>() { "Keaton", "Will", "Sarah" };
+            game.ListPlayers();
+            game.Play();
+            Console.ReadLine();
+
             Deck deck = new Deck();
             deck.Shuffle(3);
-            
-            foreach (Card card in deck.Cards) 
+
+            foreach (Card card in deck.Cards)
             {
                 Console.WriteLine(card.Face + " of " + card.Suit);
             }
