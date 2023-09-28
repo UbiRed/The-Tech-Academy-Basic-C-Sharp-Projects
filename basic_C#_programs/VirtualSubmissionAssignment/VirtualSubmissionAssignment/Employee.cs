0e7b4d4f5c49506665b145b9f1e664b4fcd23745
Keaton K <keatonkupillas@yahoo.com>
2023-09-27 21:26:17 -0700

    update virtual

--- basic_C#_programs/VirtualSubmissionAssignment/VirtualSubmissionAssignment/Employee.cs
+++ basic_C#_programs/VirtualSubmissionAssignment/VirtualSubmissionAssignment/Employee.cs
@@ -2,7 +2,7 @@
 
 class Employee : Person
 {
-    // Implement the SayName method for Employee
+    // SayName method for Employee
     public override void SayName()
     {
         Console.WriteLine($"Name: {FirstName} {LastName}");
--- basic_C#_programs/VirtualSubmissionAssignment/VirtualSubmissionAssignment/Program.cs
+++ basic_C#_programs/VirtualSubmissionAssignment/VirtualSubmissionAssignment/Program.cs
@@ -4,17 +4,16 @@ class Program
 {
     static void Main()
     {
-        // Instantiate an Employee object with firstName “Sample” and lastName “Student”
+        // Instantiate an Employee object
         Employee employee = new Employee
         {
             FirstName = "Sample",
             LastName = "Student"
         };
 
-        // Call the SayName method on the object
+        // Call the SayName method
         employee.SayName();
 
-        // Wait for user input before exiting
         Console.ReadLine();
     }
 }
