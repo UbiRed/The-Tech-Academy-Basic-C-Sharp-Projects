e85a48030a1a2f8613828731a618a0c075085413
Keaton K <keatonkupillas@yahoo.com>
2023-09-27 21:39:12 -0700

    interface polymorphism

---
+++ basic_C#_programs/POLYMORPHISMassignment/POLYMORPHISMassignment/Employee.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Employee : Person, IQuittable
+{
+    // Implement the Quit method from the IQuittable interface
+    public void Quit()
+    {
+        Console.WriteLine($"Employee {FirstName} {LastName} has quit.");
+    }
+}
---
+++ basic_C#_programs/POLYMORPHISMassignment/POLYMORPHISMassignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main()
+    {
+        // Create an Employee object
+        Employee employee = new Employee
+        {
+            FirstName = "John",
+            LastName = "Doe"
+        };
+
+        // Create an IQuittable reference and assign it to the Employee object
+        IQuittable quittableEmployee = employee;
+
+        // Call the Quit method using polymorphism
+        quittableEmployee.Quit();
+
+        // Wait for user input before exiting
+        Console.ReadLine();
+    }
+}
