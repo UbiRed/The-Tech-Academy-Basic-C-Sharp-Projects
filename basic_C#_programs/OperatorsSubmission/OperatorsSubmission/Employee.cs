e649ebd74ad5ee6931365b93e81b8d871f63a159
Keaton K <keatonkupillas@yahoo.com>
2023-09-28 16:03:20 -0700

    operators submission

---
+++ basic_C#_programs/OperatorsSubmission/OperatorsSubmission/Employee.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class Employee
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+    // Overload "==" operator to compare employees by Id
+    public static bool operator ==(Employee employee1, Employee employee2)
+    {
+        // Check if both objects are null or if they have the same reference
+        if (ReferenceEquals(employee1, employee2))
+        {
+            return true;
+        }
+
+        // Check if either object is null, then they are not equal
+        if (ReferenceEquals(employee1, null) || ReferenceEquals(employee2, null))
+        {
+            return false;
+        }
+
+        // Compare employees by their Id
+        return employee1.Id == employee2.Id;
+    }
+
+    // Overload "!=" operator to appease the "=="
+    public static bool operator !=(Employee employee1, Employee employee2)
+    {
+        return !(employee1 == employee2);
+    }
+}
---
+++ basic_C#_programs/OperatorsSubmission/OperatorsSubmission/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Instantiate two Employees
+        Employee employee1 = new Employee
+        {
+            Id = 1,
+            FirstName = "John",
+            LastName = "Doe"
+        };
+
+        Employee employee2 = new Employee
+        {
+            Id = 2,
+            FirstName = "Jane",
+            LastName = "Smith"
+        };
+
+        // Compare the Employees using overloaded "==" 
+        bool areEqual = employee1 == employee2;
+
+        // Display the result
+        Console.WriteLine("Are the employees the samew? " + areEqual);
+        Console.ReadLine();
+    }
+}
