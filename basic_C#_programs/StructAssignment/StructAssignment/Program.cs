12fe5ddf5b2719a863383e8f16622c1386c82237
Keaton K <keatonkupillas@yahoo.com>
2023-09-29 20:21:31 -0700

    strust assignment and more blackjack

---
+++ basic_C#_programs/StructAssignment/StructAssignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+// Define a struct, Number
+struct Number
+{
+    // Property Amount of data type decimal
+    public decimal Amount { get; set; }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Create an object of data type Number
+        Number number = new Number();
+
+        // Assign a number to it
+        number.Amount = 123.45M;
+
+        // Print the amount to the console
+        Console.WriteLine($"The amount is: {number.Amount}");
+        Console.ReadLine();
+    }
+}
