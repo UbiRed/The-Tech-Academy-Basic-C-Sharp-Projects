978721bd096da8cb45d7b55b2c04984c807c74b7
Keaton K <keatonkupillas@yahoo.com>
2023-10-03 11:52:40 -0700

    timestamps for 21 logs and datetime assignment

---
+++ basic_C#_programs/DateTimeAssignment/DateTimeAssignment/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace DateTimeCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Print date and time
+            DateTime currentDateTime = DateTime.Now;
+            Console.WriteLine($"Current Date and Time: {currentDateTime}");
+
+            // Ask user for a number
+            Console.Write("Please enter a number: ");
+            string userInput = Console.ReadLine();
+
+            // Attempt to parse the user's input into an integer
+            if (int.TryParse(userInput, out int hoursToAdd))
+            {
+                // Calculate the future date and time
+                DateTime futureDateTime = currentDateTime.AddHours(hoursToAdd);
+                Console.WriteLine($"In {hoursToAdd} hours, it will be: {futureDateTime}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number.");
+            }
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
--- basic_C#_programs/TwentyOne/TwentyOne/Dealer.cs
+++ basic_C#_programs/TwentyOne/TwentyOne/Dealer.cs
@@ -20,6 +20,7 @@ namespace TwentyOne
             Console.WriteLine(card);
             using (StreamWriter file = new StreamWriter(@"C:\Users\keato\Documents\GitHub\The-Tech-Academy-Basic-C-Sharp-Projects\basic_C#_programs\TwentyOne\Logs\log.txt", true))
             {
+                file.WriteLine(DateTime.Now);
                 file.WriteLine(card);
             }
             Deck.Cards.RemoveAt(0);
