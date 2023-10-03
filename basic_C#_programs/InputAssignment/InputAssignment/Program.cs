863bd9010d43db6882593c26197b2ef2568349bb
Keaton K <keatonkupillas@yahoo.com>
2023-10-03 11:41:42 -0700

    LOGS IN 21 AND INPUT ASSIGNMENT

---
+++ basic_C#_programs/InputAssignment/InputAssignment/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace NumberLogger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ask the user for a number
+            Console.Write("Please enter a number: ");
+            string userInput = Console.ReadLine();
+
+            // Define the file path
+            string filePath = @"C:\Users\keato\Documents\Logs\log.txt";
+
+            
+            // Write the user's input to the text file
+            using (StreamWriter writer = File.AppendText(filePath))
+            {
+                writer.WriteLine(userInput);
+            }
+
+            // Read and print the content of the text file
+            string[] lines = File.ReadAllLines(filePath);
+            Console.WriteLine("Numbers in the log file:");
+            foreach (string line in lines)
+            {
+                Console.WriteLine(line);
+            }
+            
+            
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
